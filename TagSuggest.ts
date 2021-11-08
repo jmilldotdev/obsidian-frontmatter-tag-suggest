ffdfc0e7da8bc3998317137abead165e3da61436
Jonathan Miller <jon@jmill.dev>
2021-11-08 15:47:56 -0500

    autocomplete first tag working

--- TagSuggest.ts
+++ TagSuggest.ts
@@ -4,20 +4,23 @@ import {
 	EditorSuggest,
 	EditorSuggestContext,
 	EditorSuggestTriggerInfo,
+	getAllTags,
 	TFile,
 } from "obsidian";
 import FrontmatterTagSuggestPlugin from "./main";
 
 export default class TagSuggest extends EditorSuggest<string> {
 	plugin: FrontmatterTagSuggestPlugin;
-	emoji = {
-		":100:": "💯",
-		":1234:": "🔢",
-	};
+	tags: string[];
 
 	constructor(plugin: FrontmatterTagSuggestPlugin) {
 		super(plugin.app);
 		this.plugin = plugin;
+		this.tags = getAllTags(
+			this.plugin.app.metadataCache.getFileCache(
+				this.plugin.app.workspace.getActiveFile()
+			)
+		).map((p) => p.split("#").pop());
 	}
 
 	onTrigger(
@@ -36,31 +39,30 @@ export default class TagSuggest extends EditorSuggest<string> {
 				},
 				query: match,
 			};
-			console.log(matchData);
 			return matchData;
 		}
 		return null;
 	}
 
 	getSuggestions(context: EditorSuggestContext): string[] {
-		return Object.keys(this.emoji).filter((p) =>
+		console.log(context.query);
+		console.log(this.tags);
+		const suggestions = this.tags.filter((p) =>
 			p.startsWith(context.query)
 		);
+		console.log(suggestions);
+		return suggestions;
 	}
 
 	renderSuggestion(suggestion: string, el: HTMLElement): void {
 		const outer = el.createDiv({ cls: "ES-suggester-container" });
-		outer
-			.createDiv({ cls: "ES-shortcode" })
-			.setText(suggestion.replace(/:/g, ""));
-		//@ts-expect-error
-		outer.createDiv({ cls: "ES-emoji" }).setText(this.emoji[suggestion]);
+		outer.createDiv({ cls: "ES-emoji" }).setText(suggestion);
 	}
 
 	selectSuggestion(suggestion: string): void {
 		if (this.context) {
 			(this.context.editor as Editor).replaceRange(
-				`${suggestion}`,
+				`${suggestion} `,
 				this.context.start,
 				this.context.end
 			);
