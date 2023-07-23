7428f54e9a1a460fb6947c17510343031744b05f
Valerie Burzynski <valerie@bendyworks.com>
2023-07-23 13:06:29 -0500

    feature: add settings menu

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 import {
+	App,
+	PluginSettingTab,
+	Setting,
 	Editor,
 	EditorPosition,
 	EditorSuggest,
@@ -6,21 +9,51 @@ import {
 	EditorSuggestTriggerInfo,
 	Plugin,
 	TFile,
+	ToggleComponent,
+	TextComponent,
 } from "obsidian";
 
+interface FrontmatterTagSuggestPluginSettings {
+	addNewEntry: boolean;
+	useSpaces: boolean;
+	numSpaces: number;
+}
+
+const DEFAULT_SETTINGS: FrontmatterTagSuggestPluginSettings = {
+	addNewEntry: true,
+	useSpaces: true,
+	numSpaces: 1,
+}
+
 export default class FrontmatterTagSuggestPlugin extends Plugin {
+	settings: FrontmatterTagSuggestPluginSettings
+
 	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new Settings(this.app, this));
 		this.registerEditorSuggest(new TagSuggest(this));
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 }
 
 class TagSuggest extends EditorSuggest<string> {
 	plugin: FrontmatterTagSuggestPlugin;
 	tags: string[];
+	indent: string;
 
 	constructor(plugin: FrontmatterTagSuggestPlugin) {
 		super(plugin.app);
 		this.plugin = plugin;
+
+		const { useSpaces, numSpaces } = plugin.settings;
+		this.indent = useSpaces ? (" ").repeat(numSpaces) : "\t";
 	}
 
 	getTags(): string[] {
@@ -28,6 +61,7 @@ class TagSuggest extends EditorSuggest<string> {
 		const tags: any = this.plugin.app.metadataCache.getTags();
 		return [...Object.keys(tags)].map((p) => p.split("#").pop());
 	}
+
 	inRange(range: string) {
 		if (!range || !range.length) return false;
 		if (range.match(/^---\n/gm)?.length != 1) return false;
@@ -86,10 +120,12 @@ class TagSuggest extends EditorSuggest<string> {
 
 	selectSuggestion(suggestion: string): void {
 		if (this.context) {
-			if (this.inline) {
-				suggestion = `${suggestion}`;
-			} else {
-				suggestion = `${suggestion}\n -`;
+			if (this.plugin.settings.addNewEntry) {
+				if (this.inline) {
+					suggestion = `${suggestion}, `;
+				} else {
+					suggestion = `${suggestion}\n${this.indent}-`;
+				}
 			}
 			(this.context.editor as Editor).replaceRange(
 				`${suggestion} `,
@@ -97,5 +133,56 @@ class TagSuggest extends EditorSuggest<string> {
 				this.context.end
 			);
 		}
+	}
+}
+
+class Settings extends PluginSettingTab {
+	plugin: FrontmatterTagSuggestPlugin;
+
+	constructor(app: App, plugin: FrontmatterTagSuggestPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Automatically add another item in multi-line")
+			.setDesc("When enabled, ")
+			.addToggle((toggle: ToggleComponent) => toggle
+				.setValue(this.plugin.settings.addNewEntry)
+				.onChange(async (value: boolean) => {
+					this.plugin.settings.addNewEntry = value;
+					await this.plugin.saveSettings();
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Use Spaces for Indentation")
+			.setDesc("If enabled, the next multi-line tag will be indented with spaces rather than a tab")
+			.addToggle((toggle: ToggleComponent) => toggle
+				.setValue(this.plugin.settings.useSpaces)
+				.onChange(async (value: boolean) => {
+					this.plugin.settings.useSpaces = value;
+					await this.plugin.saveSettings();
+				})
+			);
+
+		new Setting(containerEl)
+			.setName("Number of Spaces for Indentation")
+			.setDesc("If use spaces is enabled, this is the number of spaces that will be used for indentation")
+			.addText((text: TextComponent) => text
+				.setPlaceholder("1")
+				.setValue("" + this.plugin.settings.numSpaces)
+				.onChange(async (value: string) => {
+					let num = parseInt(value, 10);
+					if (isNaN(num)) return;
+					this.plugin.settings.numSpaces = num;
+					await this.plugin.saveSettings();
+				})
+			);
 	}
 }
